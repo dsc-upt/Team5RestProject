5e6142359531d0db5f9ca4db49c8bef7fa279c37
Tudor Ciutacu <tudor.ciutacu@gmail.com>
2022-02-15 16:47:21 +0200

    made model

--- backend/API/Models/ExampleModel.py
+++ backend/API/Models/ExampleModel.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    Title = models.CharField(max_length=256)
+    Description = models.TextField()
+    URL = models.URLField()
+    Category = models.ForeignKey()
